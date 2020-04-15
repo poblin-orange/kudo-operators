013f9af7c30250bd5e417d552ceddc7211e25567
Pierre Oblin <pierre.oblin@orange.com>
2020-04-15 12:37:50 +0000

    add plan command

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #kubectl kudo init
 kubectl kudo install ./gitea-operator/operator
+kubectl kudo plan  status --instance=gitea-operator-instance
 #kubectl kudo upgrade --instance=gitea-operator-instance ./gitea-operator/operator
